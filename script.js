ba44feff2ec6b57926e105cd05b07b3ff908f9b3
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-11-07 22:07:40 +0500

    Bug fixed

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ const cron = require('node-cron');
 const fs = require('fs');
 
 const token = '6487757037:AAFiPXFXVB8NhAWWT0EXzeHKKuVJgPpZBL0'; // Telegram botning tokeni
-const channelId = '@YOUR_CHANNEL_NAME'; // Kanalni nomi yoki idsi
-const groupId = '-738563273'; // Guruh ID'si
+const groupId1 = '-738563273'; // Birinchi guruh ID'si
+// const groupId2 = 'Rassom4444'; // Ikkinchi guruh ID'si
+// const groupId3 = 'mulkushyoshlari11111'; // Uchinchi guruh ID'si
 const bot = new TelegramBot(token);
 
-// Har soatda boshlash uchun =>| 0 * * * * |
-cron.schedule('0 * * * *', () => {
- 
-  // const message = 'Bu habar har soatda yuborilmoqda!';
+// 3 ta guruh uchun har 3 soatda boshlash uchun
+cron.schedule('0 */3 * * *', () => {
   const image = `./rasm.png`; // Rasm fayli manzili
   const caption = '';
 
-  
-  // bot.sendPhoto(channelId, fs.readFileSync(image), { caption: caption });
-  bot.sendPhoto(groupId, fs.readFileSync(image), { caption: caption });
-  // bot.sendMessage(channelId, message);
-
+  bot.sendPhoto(groupId1, fs.readFileSync(image), { caption: caption });
+  // bot.sendPhoto(groupId2, fs.readFileSync(image), { caption: caption });
+  // bot.sendPhoto(groupId3, fs.readFileSync(image), { caption: caption });
 });
